49e5744d29c380580600c57eab4c747e988f9ca8
Diogo Biazus <diogo@biazus.ca>
2021-03-10 16:33:33 -0500

    Add test infrastructure. We now have an http server that opens a static page simulating the linkedin sent invitations. The idea is to instrument the simulated page with test cases that will expect the correct click events.

--- src/invitationsSent.js
+++ src/invitationsSent.js
@@ -1,18 +1,33 @@
-const refs = document.querySelectorAll(`[class*="time-badge time-ago"]`);
+const re = /(\d+) months ago/
 
-refs.forEach((el) => {
-    const re = /(\d+) months ago/
+const oldEnough = (el) => {
     const monthsAgoMatch = el.innerHTML.match(re)
     if(monthsAgoMatch && monthsAgoMatch.length && monthsAgoMatch.length > 1) {
         const monthsAgo = parseInt(monthsAgoMatch[1])
-        const invitationCard = el.closest('li.invitation-card')
-        if(monthsAgo >= 3 && invitationCard) {
-            const withdrawButton = invitationCard.querySelector('button[data-control-name=withdraw_single]')
-            if(withdrawButton) {
-                withdrawButton.click()
-                const confirm = document.querySelector('button[data-test-dialog-primary-btn]')
-                if(confirm) confirm.click()
-            }
-        }
+        return monthsAgo >= 3
     }
-})
+    return false
+}
+
+const hasWithdrawButton = (el) => {
+    const invitationCard = el.closest('li.invitation-card')
+    if(invitationCard) {
+        const withdrawButton = invitationCard.querySelector('button[data-control-name=withdraw_single]')
+        return !R.isNil(withdrawButton )
+    }
+    return false
+}
+
+const filterCondition = R.allPass([oldEnough, hasWithdrawButton])
+
+const withdrawButtons = R.filter(filterCondition)
+
+const timeBadges = document.querySelectorAll(`[class*="time-badge time-ago"]`)
+
+const withdraw = (withdrawButton) => {
+    withdrawButton.click()
+    const confirm = document.querySelector('button[data-test-dialog-primary-btn]')
+    if(confirm) confirm.click()
+}
+
+withdrawButtons(timeBadges).forEach(withdraw)
